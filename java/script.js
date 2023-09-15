5df5f3714af2774bc2aa01f8543ad3262be4433d
Kim <khughes949@insite.4cd.edu>
2023-09-14 19:17:07 -0700

    Added Interactive menu for mobile platforms

--- java/script.js
+++ java/script.js
@@ -14,4 +14,25 @@ else if(Answer==="Yes"){
 else {
     console.log("You are NOT a DVC Instructor or Student! Leave my site AT ONCE!");
 }
+
+(function () {
+    "use strict";
+
+    //This function simply toggles the existence of 
+    //the "show" class on the main navigation. CSS transitions do the rest.
+    function toggleMenu() {
+        let el = document.querySelector('.main-nav');
+        el.classList.toggle('show');
+    }
+
+    //there are two buttons that open/close the menu, both classed as .menu-btn
+    //find all of them and add the interactivity to each button.
+    let menu = document.querySelectorAll('.menu-btn');
+    for (let item of menu) {
+        item.addEventListener('click', toggleMenu);
+    }
+    
+})();
+
+
       
