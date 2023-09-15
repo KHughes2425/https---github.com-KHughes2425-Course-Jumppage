7e404da4483b3e0a199371a9707f37f57dafac7a
Kim <khughes949@insite.4cd.edu>
2023-09-14 19:40:48 -0700

    added alert

--- java/script.js
+++ java/script.js
@@ -1,20 +1,4 @@
-let Answer = "Yes";
-let num2=88;
-const num3= 100;
-
-window.prompt('Are you the ARTDM 174 Instructor or Fellow Classmate?');
-if(Answer !== "Yes"){
-    let output = docuemt.getElementById("output")
-    output.innerHTML="Who are you and who sent you?!";
-}
-else if(Answer==="Yes"){
-    window.prompt("Who are you and who sent you?!")
-}
-
-else {
-    console.log("You are NOT a DVC Instructor or Student! Leave my site AT ONCE!");
-}
-
+alert("Hello and Welcome to my Jump-page!")
 (function () {
     "use strict";
 
